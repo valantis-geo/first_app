22f54c461644580c509cb7388150a1713dcd65c1
Valantis <balandisg@yahoo.gr>
2014-03-21 14:00:53 +0200

    demo1

---
+++ app/controllers/index_controller_controller.rb
@@ -0,0 +1,4 @@
+class IndexControllerController < ApplicationController
+  def index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo1::Application.config.secret_key_base = '742bfbc32f062f12a587df68fa466baed9c69c81e46694bade97785f60b4c0e9db2d375dedb24a9e019596234f125bbca6a11aa94e8798a9ce5517e3bd0ccb08'
